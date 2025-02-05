2402ee9004232772091637fd17bfd23bd9dde61f
David Essien <davidessienshare@gmail.com>
2025-02-05 11:15:37 +0100

    update script for distributing files

--- k8s-installation/k8s-hard-vagrant-bash/scripts/08-distribute-files-and-configs.sh
+++ k8s-installation/k8s-hard-vagrant-bash/scripts/08-distribute-files-and-configs.sh
@@ -2,75 +2,32 @@
 
 cp kubect /usr/local/bin/kubectl
 
-# Distribute binaries to master and worker nodes
-cd ./downloads/ || exit
-for instance in worker01 worker02; do
-  scp \
-    kubelet \
-    kube-proxy \
-    kubectl \
-    crictl-v1.28.0-linux-arm.tar.gz \
-    containerd-1.7.8-linux-arm64.tar.gz \
-    cni-plugins-linux-arm64-v1.3.0.tgz \
-    runc \
-    vagrant@${instance}:~/
-done
-
-for instance in master01 master02; do
-  scp kube-apiserver kube-controller-manager kube-scheduler kubectl etcd etcdctl vagrant@${instance}:~/
-done
-
-cd ../
-
-#Distribute certs to master and worker nodes
-cd ./certs || exit
-
-for instance in master01 master02; do
-  scp ca.crt ca.key kube-apiserver.crt kube-apiserver.key \
-    service-account.key service-account.crt etcd-server.key etcd-server.crt \
-    vagrant@${instance}:~/
-done
-
-for instance in worker01 worker02; do
-  scp ca.crt ${instance}.crt ${instance}.key  vagrant@${instance}:~/
-done
-
-cd ../
-
-
-## Distribute kube configs
-cd kube-configs || exit
-
-for instance in worker01 worker02; do  
-  scp kube-proxy.kubeconfig ${instance}.kubeconfig \
-    ../kubeconfigs/kubelet-config.yaml ../kubeconfigs/kube-proxy-config.yaml vagrant@${instance}:~/  
-done
-
-for instance in master01 master02; do
-  scp admin.kubeconfig kube-controller-manager.kubeconfig kube-scheduler.kubeconfig vagrant@${instance}:~/
-done
-
-cd ../
-
-#Distribute Service Files
+# distribute files and binaries to master notes
 for instance in master01 master02; do
   scp \
+    downloads/{kube-apiserver,kube-controller-manager,kube-scheduler,kubectl,etcd,etcdctl} \
+    certs/{ca,kube-apiserver,service-account,etcd-server}.crt \
+    certs/{ca,kube-apiserver,service-account,etcd-server}.key \
+    kube-configs/{admin,kube-controller-manager,kube-scheduler}.kubeconfig \
     ../config/systemd-units/{kubeapi-server,etcd,kubelet,kube-controller-manager,kube-scheduler}.service \
-    vagrant@"${instance}":~/
+    encryption-config.yaml \
+    vagrant@${instance}:~/
 done
 
-for instance in master01 master02; do
+# Distribute files and binaries to worker nodes
+for instance in worker01 worker02; do
   scp \
+    downloads/{kubelet,kube-proxy,kubectl,runc} \
+    downloads/{crictl-v1.32.0-linux-amd64,containerd-2.0.2-linux-amd64}.tar.gz \
+    downloads/cni-plugins-linux-arm64-v1.3.0.tgz \
+    certs/{ca,${instance}}.crt \
+    certs/${instance}.key \
+    ../kube-configs/{kube-proxy,${instance}}.kubeconfig \
+    ../kubeconfigs/{kubelet,kube-proxy}-config.yaml \
     ../config/systemd-units/{kubelet,kube-proxy,containerd}.service \
-    vagrant@"${instance}":~/
+    vagrant@${instance}:~/
 done
 
-
-# Distribute encryption keys
-for instance in master01 master02; do
-  scp encryption-config.yaml vagrant@${instance}:~/
-done
 
 # Distribute loadbalancer config
-echo "Move haproxy config to loadbalancer instance"
 scp config/haproxy.cfg vagrant@loadbalancer:~/
