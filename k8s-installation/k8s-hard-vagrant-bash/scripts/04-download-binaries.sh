bf0bc2ba2ffe6f046dcf67df8c2e9be1ac941c12
David Essien <davidessienshare@gmail.com>
2025-02-05 09:44:21 +0100

    update the download binaries script

--- k8s-installation/k8s-hard-vagrant-bash/scripts/04-download-binaries.sh
+++ k8s-installation/k8s-hard-vagrant-bash/scripts/04-download-binaries.sh
@@ -17,31 +17,20 @@ wget -q --show-progress --https-only --timestamping \
   "https://github.com/etcd-io/etcd/releases/download/v3.5.18/etcd-v3.5.18-linux-amd64.tar.gz"
 
 {
-  mkdir -p containerd cni
-  tar -xvf crictl-v1.28.0-linux-arm.tar.gz
-  tar -xvf containerd-1.7.8-linux-arm64.tar.gz -C containerd
   tar -xvf etcd-v3.5.18-linux-amd64.tar.gz
-  tar -xvf cni-plugins-linux-arm64-v1.3.0.tgz -C cni
-  mv runc.arm64 runc
-
+  mv runc.amd64 runc
   ls -al
 }
 
-rm -rf ./*.gx ././*.tgz etcd-v3.5.18-linux-amd64
-chmod +x ./*
+rm etcd-v3.5.18-linux-amd64.tar.gz
 
-for instance in worker01 worker02; do
-  scp kubelet kube-proxy kubectl vagrant@${instance}:~/
+binaries=(
+  kubectl kube-apiserver kube-controller-manager kube-scheduler 
+  kube-proxy kubelet runc etcd etcdctl
+)
+
+for binary in "${binaries[@]}"; do
+  chmod +x ./"${binary}"
 done
-
-## Distribute to masters
-for instance in master01 master02; do
-  scp kube-apiserver kube-controller-manager kube-scheduler kubectl etcd etcdctl vagrant@${instance}:~/
-done
-
-cp kubect /usr/local/bin/kubectl
-
-echo "Move haproxy config to loadbalancer instance"
-scp config/haproxy.cfg vagrant@loadbalancer:~/
 
 cd ../
