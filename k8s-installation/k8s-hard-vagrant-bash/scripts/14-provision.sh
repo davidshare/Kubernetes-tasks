dcc5972a7c788eb623081bdedc07a3a65cc169b7
David Essien <davidessienshare@gmail.com>
2025-02-08 10:38:07 +0100

    update vagrant file and scripts

--- k8s-installation/k8s-hard-vagrant-bash/scripts/14-provision.sh
+++ k8s-installation/k8s-hard-vagrant-bash/scripts/14-provision.sh
@@ -4,7 +4,7 @@ echo "Starting automated provisioning sequence..."
 vagrant provision jumpbox --provision-with distribute-files
 
 vagrant provision master01 master02 --provision-with setup-controller
-vagrant provision worker01 worker02 --provision-with setup-vm,setup-worker
+vagrant provision worker01 worker02 --provision-with setup-worker
 vagrant provision loadbalancer --provision-with setup-lb
 
 vagrant provision jumpbox --provision-with setup-networking
