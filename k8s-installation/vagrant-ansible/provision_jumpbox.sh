4aa1487c2146531b04b3e9087b0e5ece508a83fb
David Essien <davidessienshare@gmail.com>
2025-09-19 18:58:00 +0100

    update ansible playbook calls

--- k8s-installation/vagrant-ansible/provision_jumpbox.sh
+++ k8s-installation/vagrant-ansible/provision_jumpbox.sh
@@ -22,18 +22,4 @@ task_echo "[Task 2] - Verify directory structure"
     ls -la /home/vagrant/k8s-project/ansible/ || echo "Ansible directory not found!"
 }
 
-task_echo "[Task 3] - Run Ansible playbook for jumpbox setup"
-{
-    # Check if ansible directory exists
-    if [ -d "/home/vagrant/k8s-project/ansible" ]; then
-        cd /home/vagrant/k8s-project/ansible
-        ansible-playbook -i inventory/hosts.ini playbooks/jumpbox_setup.yml --connection=local -l jumpbox
-    else
-        echo "ERROR: Ansible directory not found at /home/vagrant/k8s-project/ansible/"
-        echo "Available content in /home/vagrant/k8s-project/:"
-        ls -la /home/vagrant/k8s-project/
-        exit 1
-    fi
-}
-
 echo "Jumpbox provisioning complete."
